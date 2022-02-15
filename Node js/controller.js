218b5f8fa454163dd8d7c05a1c407a1b40da9059
rishab-suresh <56022095+rishab-suresh@users.noreply.github.com>
2022-02-15 17:21:54 +0530

    Add files via upload

---
+++ Node js/controller.js
@@ -0,0 +1,69 @@
+const data = require("./data");
+
+class Controller {
+    
+    async getTodos() {
+        
+        return new Promise((resolve, _) => resolve(data));
+    }
+
+    
+    async getTodo(id) {
+        return new Promise((resolve, reject) => {
+            
+            let todo = data.find((todo) => todo.id === parseInt(id));
+            if (todo) {
+                
+                resolve(todo);
+            } else {
+                
+                reject(`Todo with id ${id} not found `);
+            }
+        });
+    }
+
+    // creating a todo
+    async createTodo(todo) {
+        return new Promise((resolve, _) => {
+            // create a todo, with random id and data sent
+            let newTodo = {
+                id: Math.floor(4 + Math.random() * 10),
+                ...todo,
+            };
+
+            // return the new created todo
+            resolve(newTodo);
+        });
+    }
+
+    // updating a todo
+    async updateTodo(id) {
+        return new Promise((resolve, reject) => {
+            // get the todo.
+            let todo = data.find((todo) => todo.id === parseInt(id));
+            // if no todo, return an error
+            if (!todo) {
+                reject(`No todo with id ${id} found`);
+            }
+            //else, update it by setting completed to true
+            todo["completed"] = true;
+            // return the updated todo
+            resolve(todo);
+        });
+    }
+
+    // deleting a todo
+    async deleteTodo(id) {
+        return new Promise((resolve, reject) => {
+            // get the todo
+            let todo = data.find((todo) => todo.id === parseInt(id));
+            // if no todo, return an error
+            if (!todo) {
+                reject(`No todo with id ${id} found`);
+            }
+            // else, return a success message
+            resolve(`Todo deleted successfully`);
+        });
+    }
+}
+module.exports = Controller;
---
+++ Node js/data.js
@@ -0,0 +1,27 @@
+const todos = [
+    {
+        id: 1,
+        title: "test1",
+        description: "First test",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "tes2",
+        description: "Second Test",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "test3",
+        description: "Thitd",
+        completed: false,
+    },
+    {
+        id: 4,
+        title: "test4",
+        description: "fourth",
+        completed: false,
+    },
+];
+module.exports = todos;
